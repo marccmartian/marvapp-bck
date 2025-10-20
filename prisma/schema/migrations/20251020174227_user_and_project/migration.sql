9bf6db932615d8ea93b7cf291bdf4877e6e2b993
Marvin <marvincarrascoc@gmail.com>
2025-10-20 13:27:30 -0500

    delete category table

--- prisma/schema/migrations/20251020174227_user_and_project/migration.sql
+++ prisma/schema/migrations/20251020174227_user_and_project/migration.sql
@@ -1,14 +1,5 @@
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
-
--- CreateTable
-CREATE TABLE "Category" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "status" BOOLEAN NOT NULL DEFAULT true,
-
-    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
-);
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'EDITOR', 'VIEWER');
 
 -- CreateTable
 CREATE TABLE "Project" (
@@ -16,6 +7,7 @@ CREATE TABLE "Project" (
     "userId" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "keywords" TEXT[],
     "imageUrl" TEXT,
     "githubUrl" TEXT NOT NULL,
     "prodUrl" TEXT NOT NULL,
@@ -34,7 +26,7 @@ CREATE TABLE "User" (
     "surname" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" "Role" NOT NULL DEFAULT 'USER',
+    "role" "Role" NOT NULL DEFAULT 'VIEWER',
     "avatar" TEXT,
     "isEmailValidated" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -43,22 +35,11 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "_CategoryToProject" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL,
-
-    CONSTRAINT "_CategoryToProject_AB_pkey" PRIMARY KEY ("A","B")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
-
 -- CreateIndex
 CREATE UNIQUE INDEX "Project_title_key" ON "Project"("title");
 
 -- CreateIndex
-CREATE INDEX "Project_title_description_idx" ON "Project"("title", "description");
+CREATE INDEX "Project_title_description_keywords_idx" ON "Project"("title", "description", "keywords");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
@@ -66,14 +47,5 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 -- CreateIndex
 CREATE INDEX "User_name_surname_email_idx" ON "User"("name", "surname", "email");
 
--- CreateIndex
-CREATE INDEX "_CategoryToProject_B_index" ON "_CategoryToProject"("B");
-
 -- AddForeignKey
 ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_CategoryToProject" ADD CONSTRAINT "_CategoryToProject_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_CategoryToProject" ADD CONSTRAINT "_CategoryToProject_B_fkey" FOREIGN KEY ("B") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
